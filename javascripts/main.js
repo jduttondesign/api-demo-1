11e66c5e2ee787a0cfec0c8998470fb48689c472
Zoe Ames <zoeames@gmail.com>
2016-10-25 19:52:59 -0500

    correctly connecting to imgur

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,43 @@
 "use strict";
 
+let apiKeys = {};
+
+let imageList = (searchText) => {
+  return new Promise ((resolve, reject) => {
+    $.ajax({
+      method: 'GET',
+      url: 'apiKeys.json'
+    }).then((response) => {
+      apiKeys = response;
+      let authHeader = 'Client-ID ' + apiKeys.client_id;
+      $.ajax({
+        method: 'GET',
+        headers:{
+          'Authorization': authHeader
+        },
+        url: ` https://api.imgur.com/3/gallery/t/${searchText}`
+      }).then((response2)=>{
+        resolve(response2.data.items);
+      }, (errorResponse2) => {
+        reject(errorResponse2);
+      });
+    }, (errorResponse) =>{
+      reject(errorResponse);
+    });
+  });
+};
+
+
+
 $(document).ready(function() {
   console.log("jquery is ready");
+  imageList('cat').then((dataFromImgur)=>{
+    console.log('dataFromImgur', dataFromImgur);
+  });
 });
+
+
+
+
+
+
